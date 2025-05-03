644b7b9bd177079fd40feb617bae295c9443dbe9
ayush bhatt <mitul.l.fst@gmail.com>
2025-05-03 18:26:31 +0530

    first commit

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
     setError("");
 
     try {
-      const response = await fetch("http://localhost:5000/login", {
+      const response = await fetch("/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
